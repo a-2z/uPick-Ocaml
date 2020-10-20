13966c2f53ec30c13c6fbf8469620d23a8ac0367
Your Name <ao296@cornell.edu>
2020-10-20 14:26:19 -0500

    groups update

--- groups.ml
+++ groups.ml
@@ -1,4 +1,24 @@
-type t = 
-  {
+(* open Yojson.Basic.Util *)
 
-  }
+type t = {
+  users : string list;
+  restaurants : string list;
+  voting_progress : bool;
+  user_voted : bool;
+  group_voted : (string * bool) list
+}
+
+let get_users t = 
+  t.users
+
+let get_restaurants =
+  t.restaurants
+
+let voting_in_progress =
+  t.voting_progress
+
+let has_user_voted =
+  t.user_voted
+
+let has_group_voted = 
+  t.group_voted
