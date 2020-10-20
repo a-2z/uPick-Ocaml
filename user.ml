2245416e86bf04ef6b251ac29ce5b314e77a2df6
Zachary Tegtmeier <zjt5@cornell.edu>
2020-10-20 16:56:37 -0400

    Update to user.ml and user.mli before pulling

--- user.ml
+++ user.ml
@@ -1,23 +1,41 @@
+open Yojson.Basic.Util
+
 type restrictions = Dairy | Shellfish 
 
 type foods = Dairy | Shellfish | Cake | Mexican 
 
 type preference = 
-{
-	time : int;
-	dist : int;
-	price : int;
-	openness : bool;
-	restrictions : restrictions;
-}
+  {
+    time : int;
+    dist : int;
+    price : int;
+    openness : bool;
+    restrictions : restrictions;
+  }
 
 type t = 
   {
-	id : int; 
-	username : string;
-	password : string;
-	name : string; 
-	friends : int list;
-	preferences : preference;
-	visited : int list;
-	}
+    id : int; 
+    username : string;
+    password : string;
+    name : string; 
+    friends : int list;
+    preferences : preference;
+    visited : int list;
+  }
+
+let get_id t = t.id
+
+let get_username t = t.username
+
+let get_name t = t.name
+
+let get_restaurants t = t.visited
+
+let get_friends t = t.username
+
+let get_preferences t = t.preferences
+
+let get_in_group t = failwith "Unimplemented"
+
+let update_in_group b t = 
