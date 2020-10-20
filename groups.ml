16f2d63ff0dc0802a2b124953feb2c997eaebbe6
Reetu Parikh <rsp99@cornell.edu>
2020-10-20 16:52:03 -0400

    groups fixed

--- groups.ml
+++ groups.ml
@@ -1,8 +1,5 @@
-(* open Yojson.Basic.Util *)
+open Yojson.Basic.Util
 
-<<<<<<< HEAD
-  }
-=======
 type t = {
   users : string list;
   restaurants : string list;
@@ -14,15 +11,14 @@ type t = {
 let get_users t = 
   t.users
 
-let get_restaurants =
+let get_restaurants t =
   t.restaurants
 
-let voting_in_progress =
+let voting_in_progress t =
   t.voting_progress
 
-let has_user_voted =
+let has_user_voted t =
   t.user_voted
 
-let has_group_voted = 
+let has_group_voted t = 
   t.group_voted
->>>>>>> 4699f24d99a7524bcb67428a16f8ad3c32f78538
