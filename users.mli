ebc69809cb49ea7619de59ca6a02f664b42d74c3
Reetu Parikh <rsp99@cornell.edu>
2020-11-04 18:35:52 -0500

    mongodb

---
+++ users.mli
@@ -0,0 +1,21 @@
+type t
+
+val person_to_bson : t -> 'a
+
+val bson_to_person : 'a -> t
+
+val get_id : bson -> 
+
+val get_username :
+
+val get_password :
+
+val get_name :
+
+val get_friends :
+
+val get_restrictions :
+
+val get_visited :
+
+val get_groups :
