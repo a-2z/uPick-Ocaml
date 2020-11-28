4b7875dc46407229c90275ac1fbf822adf2ad572
Reetu Parikh <rsp99@cornell.edu>
2020-11-28 13:40:08 -0500

    broken

--- src/search.ml
+++ src/search.ml
@@ -23,8 +23,8 @@ type t = result list
 let user_key = "0b2c1f4d2cea4f954b70b3d12683036c"
 
 let string_of_t t = 
-  List.map (fun hd -> Printf.sprintf 
-               {|
+  List.map (fun hd -> let str = Printf.sprintf 
+{|
 {
 	"name": "%s",
 	"address": "%s",
@@ -40,20 +40,27 @@ let string_of_t t =
 }
 |} hd.name hd.address hd.cuisines hd.price 
                (String.concat "\", \"" hd.highlights) hd.rating hd.photo 
-               hd.timing hd.phone hd.reservation hd.takeout) t
+               hd.timing hd.phone hd.reservation hd.takeout in print_endline str; str) t
   |> String.concat ",\n"
   |> (fun l -> "{\"restaurants\": [\n" ^ l ^ "\n]}")
-
-let is_optional = function
-  | None -> "Not Available"
-  | Some o -> o
 
 let split_str_lst l = 
   match String.split_on_char ',' l with
   | [] -> ""
   | h :: t -> String.trim h
 
-(****TODO: IMPLEMENT OPTIONS *)
+  (**[splice n lst] returns the first [n] elements of [lst]
+
+   Requires: [lst] has at least [n] elements.*)
+let splice n lst =
+  let rec aux i l' = 
+    match i with 
+    | 5 -> l' 
+    | n -> begin match l' with 
+        | [] -> failwith "invariant violated"
+        | h :: t -> aux (n + 1) t end in 
+  aux 0 lst
+
 let to_rest json = 
   let json = member "restaurant" json in
   {
@@ -66,7 +73,7 @@ let to_rest json =
              |> member "user_rating" 
              |> member "aggregate_rating" 
              |> to_string
-             |> float_of_string;
+             |> (fun x -> 3.0);
     photo = json |> member "photos_url" |> to_string;
     timing = json |> member "timings" |> to_string;
     phone = json |> member "phone_numbers" |> to_string |> split_str_lst;
@@ -86,17 +93,34 @@ let from_body json =
     |> List.map to_rest
   with Type_error (s, _) -> failwith ("Parsing error: " ^ s)
 
-let get_rests ?num:(n = 5) loc_x loc_y range cuisine =
+let comp_price x y = 
+  Stdlib.compare x.price y.price 
+
+(**Sets a semi-arbitrary upper bound for [price].*)
+let set_bound price = begin
+  match float_of_int price with 
+  | p when p <= 70. -> p *. (1.4 -. (p -. 10.) *. 0.005)
+  | p -> p 
+  end
+  |> int_of_float
+
+let filter_results price l = 
+  let filtered = List.filter (fun x -> (x.price <= price)) l in 
+  if List.length filtered <= 5 then l |> splice 5
+  else filtered |> splice 5
+
+let get_rests ?num:(n = 20) ?cuisine:(c = []) loc_x loc_y range price =
+  let price = set_bound price in
   let hdr = add_list (init ()) 
       [("Accept", "application/json"); ("user-key", user_key)] in 
   let url = Printf.sprintf 
       {|https://developers.zomato.com/api/v2.1/search?count=%d&lat=%f&lon=%f&radius=%f&cuisines=%s&sort=rating&order=desc|} 
-      n loc_x loc_y range (String.concat "%2c" cuisine) in
-  print_endline (url);
+      n loc_x loc_y range (String.concat "%2c" c) in
   Cohttp_lwt_unix.Client.get ~headers:hdr (Uri.of_string url)
   >>= fun a -> snd a 
                |> Cohttp_lwt__.Body.to_string 
   >>= fun b -> let fmt = (from_string b 
                           |> from_body 
+                          |> filter_results price
                           |> string_of_t) in 
-  print_endline fmt; Lwt.return (from_string fmt)
+  Lwt.return (from_string fmt)
