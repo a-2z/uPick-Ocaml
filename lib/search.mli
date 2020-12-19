bb6972b87ed264dd90276c98392d892f6762adea
Reetu Parikh <rsp99@cornell.edu>
2020-12-19 02:15:48 -0500

    clean

--- lib/search.mli
+++ lib/search.mli
@@ -1,7 +1,7 @@
 (**[get_rests num cuisine loc_x Loc_y range price] returns a list of [num] 
    restaurants as a string of a json. The data in the jsons can be seen in 
    [to_rest json]*)
-val get_rests : ?num : int -> ?cuisine : string list -> float -> float -> 
+val get_rests : ?cuisine : string list -> float -> float -> 
   int -> int -> string list -> string Lwt.t
 
 (**[get_winner json id] is a string JSON representing the restaurant with rank
